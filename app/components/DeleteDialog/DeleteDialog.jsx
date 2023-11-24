7b2f6e3726326114795d35c9838159a5e13fe88c
msionut28 <moloceaionutt@gmail.com>
2023-11-24 16:52:27 +0200

    CRUD

--- app/components/DeleteDialog/DeleteDialog.jsx
+++ app/components/DeleteDialog/DeleteDialog.jsx
@@ -54,11 +54,11 @@ const DeleteDialog = ({
                     as="h3"
                     className="text-base font-semibold leading-6 text-gray-900"
                   >
-                    Delete Event
+                    Delete Task
                   </Dialog.Title>
                   <div className="mt-2">
                     <p className="text-sm text-gray-500">
-                      Are you sure you want to delete this event?
+                      Are you sure you want to delete this task?
                     </p>
                   </div>
                 </div>
--- app/task/page.jsx
+++ app/task/page.jsx
@@ -2,6 +2,8 @@
 
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/context/AuthContext";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -12,9 +14,11 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { useAuth } from "@/context/AuthContext";
 import DynamicForm from "../components/Form/Form";
-import { useRouter } from "next/navigation";
+import ProjectCard from "../components/ProjectCard/ProjectCard";
+import Loading from "../components/Loading/Loading";
+import DeleteDialog from "../components/DeleteDialog/DeleteDialog";
+
 
 export default function TaskCreator() {
   const fieldNames = [
@@ -34,12 +38,15 @@ export default function TaskCreator() {
   ];
   const [groups, setGroups] = useState([]);
   const [selectedGroup, setSelectedGroup] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [taskId, setTaskId] = useState(null)
+  const [showDeleteModal, setShowDeleteModal] = useState(false)
   const { user } = useAuth();
   const router = useRouter();
+  const backendURL = process.env.BACKEND_URL;
+  const accessToken = localStorage.getItem("access_token");
   const onSubmit = async (data) => {
-    const backendURL = process.env.BACKEND_URL;
     try {
-      const accessToken = localStorage.getItem("access_token");
       const userId = user ? user.user_id.toString() : null;
       const taskData = {
         ...data,
@@ -66,6 +73,52 @@ export default function TaskCreator() {
     console.log(e.target.value);
     setSelectedGroup(e.target.value);
   };
+  const fetchTasks = async () => {
+    const backendURL = process.env.BACKEND_URL;
+    try {
+      const accessToken = localStorage.getItem("access_token");
+      const response = await axios.get(`${backendURL}/tasks/`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      const data = response.data;
+      console.log(data);
+      setTasks(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      const response = await axios.delete(`${backendURL}/tasks/delete/${taskId}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+  
+      if (response.status === 204) {
+        router.push('/');
+      } else {
+        console.log(`Unexpected status: ${response.status}`);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const handleCardClick = (taskId) => {
+    setTaskId(taskId)
+    console.log(taskId);
+    setShowDeleteModal(true)
+
+  }
+
+  const handleCloseModal = () => {
+    setShowDeleteModal(false);
+    setTaskId(null)
+  }
+
   useEffect(() => {
     const fetchGroups = async () => {
       const backendURL = process.env.BACKEND_URL;
@@ -84,13 +137,14 @@ export default function TaskCreator() {
       }
     };
     fetchGroups();
+    fetchTasks();
     console.log(groups);
   }, []);
   return (
-    <>
+    <div>
       <Dialog>
         <DialogTrigger asChild>
-          <Button variant="outline">Add Task</Button>
+          <Button variant="outline">Add a new Task</Button>
         </DialogTrigger>
         <DialogContent className="sm:max-w-[425px]">
           <DialogHeader>
@@ -117,7 +171,21 @@ export default function TaskCreator() {
           </DialogFooter>
         </DialogContent>
       </Dialog>
-      <h1>{selectedGroup ? selectedGroup : "No Group Selected Yet!"}</h1>
-    </>
+      {tasks ? tasks.map((task) => (
+        <div className="mx-auto mt-5 w-52" key={task.id} onClick={()=> handleCardClick(task.id)}>
+          <ProjectCard
+            title={task.title}
+            description={task.description}
+            percentage={task.deadline}
+            icon="/icons/white/task.svg"
+          />
+        </div>
+      )): <Loading innertext={"Please 🐻 with us while we're getting the data from the dark side.."}/>}
+      <DeleteDialog
+              showDeleteModal={showDeleteModal}
+              setShowDeleteModal={setShowDeleteModal}
+              handleDelete={handleDelete}
+              handleCloseModal={handleCloseModal}/>
+    </div>
   );
 }
